9ed7b92477af087fa92b695cc87bf7853f968b13
ericthegeek2 <167888435+ericthegeek2@users.noreply.github.com>
2024-04-23 17:55:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Calling the function to print the numbers from 1 to 100 with FizzBuzz rules applied
+fizzbuzz()
+
+
